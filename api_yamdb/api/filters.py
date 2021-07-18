e21880e018f29e8b77f28170f8ce073d2389b056
zevs <gmalikggg@gmail.com>
2021-07-18 15:09:12 +0300

    new

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,15 +1,15 @@
-import django_filters
+from django_filters import rest_framework as filters
 
 from .models import Titles
 
 
-class TitlesFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(
+class TitlesFilter(filters.FilterSet):
+    name = filters.CharFilter(
         field_name='name', lookup_expr='icontains'
     )
-    genre = django_filters.CharFilter(field_name='genre__slug')
-    category = django_filters.CharFilter(field_name='category__slug')
-    year = django_filters.NumberFilter(field_name='year')
+    genre = filters.CharFilter(field_name='genre__slug')
+    category = filters.CharFilter(field_name='category__slug')
+    year = filters.NumberFilter(field_name='year')
 
     class Meta:
         model = Titles
