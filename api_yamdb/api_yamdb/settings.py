c2b0f398934b842c3eff431fe4a1d8126fda0afa
zevs <gmalikggg@gmail.com>
2021-07-18 14:57:51 +0300

    new

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,8 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
     'api',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
